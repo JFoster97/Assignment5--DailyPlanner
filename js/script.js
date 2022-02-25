7380c22fc3c01d3c9df12a49875b0870862a59dd
Jonah Foster <jonahfoster97@gmail.com>
2022-02-25 11:37:36 -0600

    began generating cards, starting with the hour on the left of the card

--- js/script.js
+++ js/script.js
@@ -7,3 +7,15 @@ var body = $('body');
 
 // Format date display
 date.text(today.format('dddd, MMMM Do'));
+
+for (var i = 9; i <= 17; i++) {
+    // create timeblock div
+    var timeblockEl = $('<div>');
+    timeblockEl.attr('class', 'time-block-'+i+' row');
+
+
+
+    var hourEl = $('<div>');
+    hourEl.attr('class', 'hour col-1');
+    hourEl.text(i);
+    timeblockEl.append(hourEl);
