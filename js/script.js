ffe49fcd60a390690a6e208fbf9621cd9b8a96b2
Jonah Foster <jonahfoster97@gmail.com>
2022-03-22 14:59:18 -0500

    got the past and present to display, but not future

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var notes = [""]
 
-var blocks = [".8am", ".9am", ".10am", ".11am", ".12pm", ".1pm", ".2pm", ".3pm", ".4pm", ".5pm"]
+var hourBlocks = [".8am", ".9am", ".10am", ".11am", ".12pm", ".1pm", ".2pm", ".3pm", ".4pm", ".5pm"]
 
 var currentDay = $("#currentDay").text(moment().format('ddd, MMMM D, YYYY'));
 
@@ -8,11 +8,34 @@ var time = [8, 9, 10, 11, 12, 13, 14, 15, 16, 17];
 
 var currentTime = parseInt(moment().format('H'));
 
+var hourBlocksIndex = time.indexOf(currentTime)
+
 // Create a function to add data to local storage with button
 
-$(".saveBtn").on("click", function () {
+$(".storeBtn").on("click", function () {
     var todo = $(this).data('index');
-    allNotes[todo] = $(blocks[todo]).val();
-    localStorage.setItem('notes', JSON.stringify(notes))
-    alert("Schedule Updated")
+    notes[todo] = $(hourBlocks[todo]).val();
+    localStorage.setItem('notes', JSON.stringify(notes));
+    alert("Schedule Updated");
 })
+
+// Create function to change color based off of the time of day
+
+function formatTimes() {
+    $(hourBlocks[hourBlocksIndex]).addClass("present");
+    for (i = 0; i < hourBlocksIndex; i++) {
+        $(hourBlocks[i]).addClass("past");
+    }
+    for (i = hourblocks + 1; i < hourBlocks.length; i++) {
+        $(hourBlocks[i]).addClass("future");
+    }
+}
+
+function showStorage() {
+    for (i = 0; i < hourBlocks.length; i++) {
+        ($(hourBlocks[i])).val(notes[i])
+    }
+}
+
+formatTimes();
+showStorage();
